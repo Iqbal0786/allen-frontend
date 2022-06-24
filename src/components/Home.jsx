578c05ec71574ab65ae37cae3ada0996e96aec86
Mohammad Iqbal <iqbal240456@gmail.com>
2022-06-24 23:18:22 +0530

    some updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,66 +1,119 @@
-import { CleaningServicesOutlined } from '@mui/icons-material'
-import { AppBar, Box, Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Toolbar, Typography } from '@mui/material'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import {useSelector} from "react-redux"
-import { SignIn } from './SignIn'
+import { CleaningServicesOutlined } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { SignIn } from "./SignIn";
 export default function Home() {
-    const {optionData} =useSelector((store)=>store.options)
-    const logdata= useSelector((store)=>store.logInData)
-    const [value, setValue] = React.useState('');
-    const [score,setScore]=useState(0);
+  const { optionData } = useSelector((store) => store.options);
+  const logdata = useSelector((store) => store.logInData);
+  const [value, setValue] = React.useState("");
+ // const [score, setScore] = useState(0);
+  let answer = [];
+let score=0
+  const handleAnswer = (index) => {
+    console.log(index)
 
-    const handleChange = (event) => {
-      setValue(event.target.value);
-    };
-  console.log(value)
-  console.log(score)
-  return (
-    logdata.token? <>
-   <AppBar>
-    <Toolbar>
-        <Typography>Online Test</Typography>
-    </Toolbar>
-   </AppBar>
-   <Box
-    sx={{width:"70%" ,margin:"auto",marginTop:"100px"}}
-    
-    >
-     {optionData.easy.map((e,i)=>{
-      
-      return (
-        <Box>
-        <FormControl>
-        <FormLabel id="demo-controlled-radio-buttons-group" 
-        sx={{fontFamily:"revert",fontSize:"19px" , color:"black"}}
-        >{i+1} : {e.question}</FormLabel>
-        <RadioGroup
-          aria-labelledby="demo-controlled-radio-buttons-group"
-          name="controlled-radio-buttons-group"
-          value={value}
-          onChange={handleChange}
-        >
-         {e.answers.map((q)=>{
-          return (
-            <FormControlLabel value={q} control={<Radio />} label={q} />
-          )
-         
-         })}
-         
-        </RadioGroup>
-      </FormControl>
-    
-
-      
-        </Box>
+  };
+ const getScore =()=>{
+   for(let i=0;i<optionData.easy.length;i++){
+     for(let j=0;j<answer.length;j++){
+      if(i==answer[j].questionId){
+        let temp=optionData.easy[i].correctIndex
+        // if(optionData.easy[i][temp]==answer[j].answervalue){
+          
+        //   score++
+        // }
+     //   console.log(temp)
+      if(optionData.easy[i].answers[temp]==answer[j].answervalue){
+        score++
+      }
         
-      )
-     
-     })}
-  
+      }
+     }
+   }
+   console.log(score)
+ }
+  console.log(value);
+  console.log(optionData);
+  return logdata.token ? (
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography>Online Test</Typography>
+        </Toolbar>
+      </AppBar>
 
-   </Box>
-   
-   </>:<SignIn/>
-  )
+      <Box sx={{ width: "70%", margin: "auto", marginTop: "100px" }}>
+        {optionData.easy.map((e, i) => {
+          return <SingleOption data={e} i={i} answer={answer} />;
+        })}
+        <Button variant="outlined" onClick={getScore}>Submit</Button>
+      </Box>
+    </>
+  ) : (
+    <SignIn />
+  );
 }
+
+export const SingleOption = ({ data, i, answer }) => {
+  const [selected, setSelected] = useState("");
+  
+    const handleSelect=(e)=>{
+       setSelected(e.target.value)
+          for(let j=0;j<answer.length;j++){
+             if(answer[j].questionId==i){
+              if(answer[j].answervalue==e.target.value){
+                answer.splice(j, 1);
+              }
+              else{
+                answer[j].answervalue=e.target.value
+              }
+              console.log(answer)
+              return 
+             }
+          }
+          answer.push({
+            questionId:i,
+            answervalue:e.target.value
+          })
+         console.log(answer)
+    }
+  return (
+    <>
+      <Box>
+        <FormControl>
+          <FormLabel
+            id="demo-controlled-radio-buttons-group"
+            sx={{ fontFamily: "revert", fontSize: "19px", color: "black" }}
+          >
+            {i + 1} : {data.question}
+          </FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-controlled-radio-buttons-group"
+            name="controlled-radio-buttons-group"
+            value={selected}
+            onChange={handleSelect}
+          >
+            {data.answers.map((q) => {
+              return (
+                <FormControlLabel value={q} control={<Radio />} label={q} />
+              );
+            })}
+          </RadioGroup>
+        </FormControl>
+      </Box>
+    </>
+  );
+};
